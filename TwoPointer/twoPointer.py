35c8e8921fb976a0b6d04dd9c38e3e53211317a1
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-01-07 18:29:29 +0530

    1

--- TwoPointer/twoPointer.py
+++ TwoPointer/twoPointer.py
@@ -308,3 +308,31 @@
 #             else:
 #                 j -= 1
 #         return count
+
+# -------------------------------------------------------------------------------------------------------------
+# 1. TWO SUM
+
+# BRUTEFORCE : Check each number in the array which every other element to find the pair that sums to the target.
+# COMPLEXITY ANALYSIS:
+# Time Complexity: O(N^2), where N is the number of elements in the input array.
+# Space Complexity: O(1), as we are using only a constant amount of extra space
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#         return []  # Fallback (problem guarantees a solution)
+
+# APPROACH 2: Use a hashmap to store the complement of each number (target - num) and its index. As we iterate through the array, we check if the current number exists in the hashmap. If it does, we have found the two numbers that sum to the target.
+# COMPLEXITY ANALYSIS:
+# Time Complexity: O(N), where N is the number of elements in the input array.
+# Space Complexity: O(N), as we are using a hashmap to store elements
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         hashmap={}
+#         for i, num in enumerate(nums):
+#             value=target-num
+#             if value in hashmap:
+#                 return[hashmap[value], i]
+#             hashmap[num]=i
