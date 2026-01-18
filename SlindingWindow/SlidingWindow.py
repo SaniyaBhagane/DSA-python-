96cf72da7a51ad39dba7d2fb86b4731c925151be
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-01-18 22:35:42 +0530

    219[1]

--- SlindingWindow/SlidingWindow.py
+++ SlindingWindow/SlidingWindow.py
@@ -159,3 +159,28 @@
 # Shrink from left → try smaller windows
 # Best window found → [4,3]
 # Output: 2
+
+# ------------------------------------------------------------------------------------------------------
+# 219. Contains Duplicates II:
+# https://leetcode.com/problems/contains-duplicate-ii/description/
+# Approach: For each index i, we look ahead up to k positions and check whether the same number appears again within this range. A set is used to track elements in the current window so duplicates can be detected quickly. If any duplicate is found within distance k, we return True; otherwise, after all checks, return False.
+# Complexity Analysis: Time Complexity: O(n × k) Space Complexity: O(k) 
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        n = len(nums)
+        for i in range(n):
+            seen = set()
+            for j in range(i, min(i+k-1), n):
+                if nums[j] in seen:
+                    return True
+                seen.add(nums[j])
+        return False
+# Example Walkthrough
+# Input: nums = [1, 2, 3, 1], k = 3
+# i = 0, window indices 0 → 3
+# seen = {}
+# j = 0 → add 1 → {1}
+# j = 1 → add 2 → {1,2}
+# j = 2 → add 3 → {1,2,3}
+# j = 3 → 1 already in set → duplicate found → return True
+# So the function stops early and returns True.
